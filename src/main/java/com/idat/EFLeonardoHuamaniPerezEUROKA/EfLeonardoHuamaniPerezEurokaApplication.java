99c0972c0d39cd06bb8f4f20a444e821f507bcaa
leonardo12 <huamani.facep@gmail.com>
2022-08-18 22:39:21 -0500

    eureka

---
+++ src/main/java/com/idat/EFLeonardoHuamaniPerezEUROKA/EfLeonardoHuamaniPerezEurokaApplication.java
@@ -0,0 +1,13 @@
+package com.idat.EFLeonardoHuamaniPerezEUROKA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfLeonardoHuamaniPerezEurokaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfLeonardoHuamaniPerezEurokaApplication.class, args);
+	}
+
+}
